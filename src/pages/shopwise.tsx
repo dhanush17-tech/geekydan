837a76568842821a2ee491dba8202dadfdaa0865
Dhanush Vardhan Kalaiselvan <dhanushvardhankalaiselvan@MacBook-Pro.local>
2023-12-23 15:21:53 -0700

    shopwise redirect

--- src/pages/shopwise.tsx
+++ src/pages/shopwise.tsx
@@ -1,12 +1,10 @@
 import { useEffect } from "react";
 
-const Page = () => {
+export default function Shopwise() {
   useEffect(() => {
     window.location.href =
       "https://play.google.com/store/apps/details?id=com.priceTracker.shopwise"; // The URL you want to redirect to
   }, []);
 
   return null; // Render nothing or a loading indicator because we're redirecting
-};
-
-export default Page;
+}
